a3094d7c025f76272ed24fc4074374143d5c3f75
root <root@vps431735.ovh.net>
2018-11-28 18:44:30 +0100

    ajout liste vide

--- videos_a_visualiser.php
+++ videos_a_visualiser.php
@@ -5,15 +5,21 @@ include('logo_search_menu.php');
 include('footer.php');
 
 print_LOGO_FORMSEARCH_MENU();
+
+$is_video = false;
+
 // connexion à la bdd
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
 $stmt = $pdo->prepare("SELECT * FROM video");
-if ($stmt->execute()) {
+$stmt->execute();
+
   echo "<div class='row'>
         <div class='col-lg-2'></div>
         <div class='col-lg-4'>
             <div class='container'>";
+
    while ($row = $stmt->fetch()) {
+     $is_video = true;
 
       if($row["valide"] == 1) {
           $motscle = "";
@@ -24,6 +30,7 @@ if ($stmt->execute()) {
                              Vous n\'avez pas de navigateur moderne, donc pas de balise video HTML5 !</video>';
          echo "<br /><br />";
 
+         // Va chercher les mots-clé :
          $stmt_ = $pdo->prepare("SELECT * FROM motcle WHERE id_video = ?");
          $stmt_->execute(array($row["id"]));
          while ($ligne = $stmt_->fetch()) {
@@ -35,10 +42,19 @@ if ($stmt->execute()) {
          }
 
 
-    echo "<br /><br /><br />";
+         echo "<br /><br /><br />";
       }
-   }
-   echo "</div></div></div>";
+    }
+
+    echo "</div></div></div>";
+
+if(!$is_video) {
+      echo "<div class='row'>
+        <div class='col-lg-4'></div>
+        <div class='col-lg-4'>
+            <div class='container'>";
+            echo "Il n'existe pas encore de vidéos";
+            echo "</div></div></div><br /><br />";
 }
 
 echo footer();
