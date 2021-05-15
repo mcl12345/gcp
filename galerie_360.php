e09ffb6cabc9186b7a45936b5d590da6229f6b0a
root <root@vps431735.ovh.net>
2021-05-15 20:15:46 +0200

    360°

--- galerie_360.php
+++ galerie_360.php
@@ -6,15 +6,15 @@ include('footer.php');
 print_LOGO_FORMSEARCH_MENU();
 
 echo '<br /><br />';
-echo '<iframe width="90%" height="500px" allowFullScreen="true" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://panoraven.com/embed/OSHb0s7Fj2"></iframe>';
+echo '<a href="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181010_155926.jpg" target="blank_"><img width="90%" height="500px" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181010_155926.jpg" />';
 echo '<br /><br />';
-echo '<iframe width="90%" height="500px" allowFullScreen="true" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://panoraven.com/embed/YWShBfWR5A"></iframe>';
+echo '<a href="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181010_160451.jpg" target="blank_"><img width="90%" height="500px" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181010_160451.jpg" />';
 echo '<br /><br />';
-echo '<iframe width="90%" height="500px" allowFullScreen="true" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://panoraven.com/embed/TDxYN90mXK"></iframe>';
+echo '<a href="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_102847.jpg" target="blank_"><img width="90%" height="500px" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_102847.jpg" />';
 echo '<br /><br />';
-echo '<iframe width="90%" height="500px" allowFullScreen="true" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://panoraven.com/embed/Lxi577voGo"></iframe>';
+echo '<a href="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_104335.jpg" target="blank_"><img width="90%" height="500px" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_104335.jpg" />';
 echo '<br /><br />';
-echo '<iframe width="90%" height="500px" allowFullScreen="true" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://panoraven.com/embed/dMpoxAU0F6"></iframe>';
+echo '<a href="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_111441.jpg" target="blank_"><img width="90%" height="500px" style="display:block; margin:20px auto; border:0 none; max-width:880px;box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 20px, rgba(0, 0, 0, 0.23) 0px 3px 20px;" src="https://basiliquesaintdenis.ovh/basilique-saint-denis/upload_images/PANO_20181107_111441.jpg" />';
 echo '<br /><br />';
 
 echo footer();
